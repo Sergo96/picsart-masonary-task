5fbf1a0b564aa19ff1c14961b6a55408e4e2d106
sergeykhachaturyan <nakato1995@gmail.com>
2025-01-22 21:41:34 +0400

    making virtulised grid, scroll logic

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,2 @@
-export { useDeviceType } from './useDeviceType/useDeviceType';
 export { useGetPhotosQuery } from '../modules/Masonry/hooks/useGetPhotosQuery/useGetPhotosQuery';
 export { useDebouncedValue } from './useDebouncedValue/useDebouncedValue.ts';
